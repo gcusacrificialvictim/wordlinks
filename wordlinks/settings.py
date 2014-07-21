8c0f40d801e64b7080b6433d1f59bf1ca87eb402
OpenShift guest <gcusacrificialvictim@gmail.com>
2014-07-21 15:14:53 -0400

    Static files settings for heroku

--- wordlinks/settings.py
+++ wordlinks/settings.py
@@ -3,6 +3,7 @@ import os;
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+PROJECT_PATH =  os.path.dirname(os.path.abspath(__file__))
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -66,7 +67,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'staticfiles'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -77,6 +78,8 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+
+    os.path.join(PROJECT_PATH, 'static'),
 )
 
 # List of finder classes that know how to find static files in
